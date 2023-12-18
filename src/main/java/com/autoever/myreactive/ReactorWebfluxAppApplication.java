e8d53ea7dd8b60913c458f88e022808d11fe0d40
vega2k <vega2k@kakao.com>
2023-12-18 12:52:05 +0900

    reactor webflux app init

---
+++ src/main/java/com/autoever/myreactive/ReactorWebfluxAppApplication.java
@@ -0,0 +1,13 @@
+package com.autoever.myreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorWebfluxAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactorWebfluxAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autoever/myreactive/ReactorWebfluxAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoever.myreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactorWebfluxAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
