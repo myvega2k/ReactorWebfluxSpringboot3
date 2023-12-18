64b75a28f93085ed6bc7c993d31fe0764f8effdf
vega2k <vega2k@kakao.com>
2023-12-18 13:25:10 +0900

    LabmdaTest class import update

--- src/test/java/com/autoever/myreactive/LambdaTest.java
+++ src/test/java/com/autoever/myreactive/LambdaTest.java
@@ -5,7 +5,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
