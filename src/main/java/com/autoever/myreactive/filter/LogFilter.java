1ceeae52e410241e056a669f7e69aa3ea23020d4
vega2k <vega2k@kakao.com>
2023-12-19 22:02:55 +0900

    filter update

--- src/main/java/com/autoever/myreactive/filter/LogFilter.java
+++ src/main/java/com/autoever/myreactive/filter/LogFilter.java
@@ -6,14 +6,14 @@ import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
-
 @Component
 @Slf4j
 public class LogFilter implements WebFilter {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
         long startTime = System.currentTimeMillis();
-        String path = exchange.getRequest().getURI().getPath();
+        String path = exchange.getRequest() //ServerHttpRequest
+                              .getURI().getPath();
 
         log.info("Serving '{}'", path);
 
@@ -22,7 +22,7 @@ public class LogFilter implements WebFilter {
             //exchange.getResponse().getHeaders() => HttpHeaders (Map)
             //exchange.getResponse().getHeaders().entrySet() => Set<Map.Entry<String, List<String>>>
                     exchange.getResponse().getHeaders().entrySet().forEach(e ->
-                            log.info("Response Headers '{}' : '{}'",e.getKey(),e.getValue()));
+                            log.info("Response Headers '{}' : '{}'", e.getKey(), e.getValue()));
                             log.info("Served '{}' as {} in {} ms",
                             path,
                             exchange.getResponse().getStatusCode(),
