b130b78027b4f485da5c357c2734eb4472816813
vega2k <vega2k@kakao.com>
2023-12-19 11:04:02 +0900

    advice problemdetail

--- src/main/java/com/autoever/myreactive/exception/advice/CustomAPIExceptionAdvice.java
+++ src/main/java/com/autoever/myreactive/exception/advice/CustomAPIExceptionAdvice.java
@@ -1,22 +1,34 @@
 package com.autoever.myreactive.exception.advice;
 
 import com.autoever.myreactive.exception.CustomAPIException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.net.URI;
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class CustomAPIExceptionAdvice {
-
+//    @ExceptionHandler(CustomAPIException.class)
+//    public ResponseEntity<?> handleCustomAPIException(CustomAPIException customAPIException){
+//        Map<String, Object> errorMap = new HashMap<>();
+//        errorMap.put("error message", customAPIException.getMessage());
+//        errorMap.put("status", customAPIException.getHttpStatus().value());
+//        //return ResponseEntity.ok(errorMap);
+//        return new ResponseEntity<>(errorMap, customAPIException.getHttpStatus());
+//    }
     @ExceptionHandler(CustomAPIException.class)
-    public ResponseEntity<?> handleCustomAPIException(CustomAPIException customAPIException){
-        Map<String, Object> errorMap = new HashMap<>();
-        errorMap.put("error message", customAPIException.getMessage());
-        errorMap.put("status", customAPIException.getHttpStatus().value());
-        //return ResponseEntity.ok(errorMap);
-        return new ResponseEntity<>(errorMap, customAPIException.getHttpStatus());
+    public ProblemDetail handlePostNotFoundException(CustomAPIException e) throws Exception{
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
+        problemDetail.setTitle("Customer Not Found");
+        problemDetail.setType(new URI("http://localhost:8080/problems/post-not-found"));
+        problemDetail.setProperty("errorCategory", "Generic");
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
     }
 }
