c7c9febcf29d3ea039a049b4b23d1ba1a7da5355
vega2k <vega2k@kakao.com>
2023-12-19 09:44:37 +0900

    controller add

--- src/main/java/com/autoever/myreactive/controller/R2CustomerController.java
+++ src/main/java/com/autoever/myreactive/controller/R2CustomerController.java
@@ -3,6 +3,7 @@ package com.autoever.myreactive.controller;
 import com.autoever.myreactive.entity.Customer;
 import com.autoever.myreactive.repository.R2CustomerRepository;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
